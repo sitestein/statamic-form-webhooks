2dce5917f32323fd391221abd3a37b04229fd398
Olav van Schie <olav@tapped.nl>
2022-12-20 11:36:14 +0100

    Make fields conditional

--- config/statamic-form-webhooks.php
+++ config/statamic-form-webhooks.php
@@ -8,10 +8,28 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you can specify the webhooks for the form submissions.
-    | The key is the form handle and the value is the webhook url.
+    | The key is the form handle.
     |
     */
     'webhooks' => [
-        // 'contact' => 'https://hook.eu1.make.com/loremipsum',
+
+        // form handle
+        'contact' => [
+
+            // webhook url
+            'url' => 'https://hook.eu1.make.com/loremipsum',
+
+            // which fields to send
+            // or use 'fields' => '*' for all
+            'fields' => [
+                'name',
+                'email',
+            ],
+
+            // conditions to send
+            // 'if' => [
+            //    'checkboxes_field.0' => 'subscribe',
+            // ],
+        ],
     ]
 ];
--- src/FormSubmission.php
+++ src/FormSubmission.php
@@ -7,20 +7,62 @@ use Illuminate\Support\Facades\Http;
 
 class FormSubmission
 {
+    /**
+     * @param SubmissionCreated $event
+     * @return void
+     */
     public function handle(SubmissionCreated $event)
     {
         $submission = $event->submission;
         $handle = $submission->form->handle;
 
-        // check if handle exists in config
-        if (!$webhook = config('statamic-form-webhooks.webhooks.' . $handle)) {
+        if (!config("statamic-form-webhooks.webhooks.{$handle}")) {
             return;
         }
 
-        $response = Http::post($webhook, $submission->data()->toArray());
+        $data = $submission->data();
+
+        $url = config("statamic-form-webhooks.webhooks.{$handle}.url");
+        $fields = config("statamic-form-webhooks.webhooks.{$handle}.fields", '*');
+        $conditions = config("statamic-form-webhooks.webhooks.{$handle}.if");
+
+        if (! $url) {
+            return;
+        }
+
+        if ($conditions && ! $this->conditionsMet($submission, $conditions)) {
+            return;
+        }
+
+        if ($fields && $fields !== '*') {
+            $data = $data->only($fields);
+        }
+
+        $response = Http::post($url, $data->toArray());
 
         if (function_exists('ray')) {
-            ray("[Sitestein\FormWebhooks]: Form submission '{$handle}' sent to '{$webhook}'", "Response code: {$response->status()}", "Response body: {$response->body()}");
+            ray(
+                "[Sitestein\FormWebhooks]",
+                "Form submission '{$handle}' sent to '{$url}'",
+                "Fields: {$data->toJson()}",
+                "Response code: {$response->status()}", "Response body: {$response->body()}"
+            );
         }
+    }
+
+    /**
+     * @param $submission
+     * @param $conditions
+     * @return bool
+     */
+    protected function conditionsMet($submission, $conditions)
+    {
+        foreach ($conditions as $key => $value) {
+            if (data_get($submission->data(), $key) !== $value) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
